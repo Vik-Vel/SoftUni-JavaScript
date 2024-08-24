8593b5676259c024b32cd408ef708a96a828225a
Viktoriya Veleva <vikiveleva54@gmail.com>
2024-08-24 20:59:26 +0300

    Created seventh task

---
+++ 1. JavaScript Basics/01. First steps in programming/07. ProjectsCreation.js
@@ -0,0 +1,10 @@
+function projectCreation(input){
+
+    let name = input[0];
+    let projects = input[1];
+    let hours = projects * 3;
+
+    console.log(`The architect ${name} will need ${hours} hours to complete ${projects} project/s.`)
+}
+
+projectCreation(["George ", "4 "])
