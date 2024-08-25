f89db19a8c12fd7fc9d15e0f982397d373c4c89a
Viktoriya Veleva <vikiveleva54@gmail.com>
2024-08-25 11:47:40 +0300

    01. USD to BGN  created and 09.yardGreening modified

--- 1. JavaScript Basics/01. First steps in coding/01. Lab/09. YardGreening.js
+++ 1. JavaScript Basics/01. First steps in coding/01. Lab/09. YardGreening.js
@@ -5,8 +5,8 @@ function yardGreening(input){
     let price = input[0] * kvM;
     let discount = price * 0.18;
 
-    console.log(`The final price is: ${price-discount.toFixed(2)} lv."`)
-    console.log(`The discount is: ${discount.toFixed(2)} lv.`)
+    console.log(`The final price is: ${price-discount} lv.`)
+    console.log(`The discount is: ${discount} lv.`)
 }
 
 yardGreening(["150"]);
---
+++ 1. JavaScript Basics/01. First steps in coding/02. Exercises/01. USDToBGN.js
@@ -0,0 +1,7 @@
+function usdToBgn(input){
+    const usd = 1.79549;
+
+    console.log(input[0] * usd);
+}
+
+usdToBgn(["22"]);
