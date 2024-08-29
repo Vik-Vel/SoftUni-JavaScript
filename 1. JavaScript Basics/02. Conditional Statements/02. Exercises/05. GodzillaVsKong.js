722d87d05fd574840615e10266f20a908f8d4477
Viktoriya Veleva <vikiveleva54@gmail.com>
2024-08-29 19:27:37 +0300

    05. Godzilla vs. Kong

---
+++ 1. JavaScript Basics/02. Conditional Statements/02. Exercises/05. GodzillaVsKong.js
@@ -0,0 +1,37 @@
+function godzillaVsKong(input){
+    let budget = Number(input[0]);
+    let numOfStatists = Number(input[1]);
+    let priceClothPerStatist = Number(input[2]);
+    let decor = budget * 0.1;
+    
+    
+
+    let price = numOfStatists * priceClothPerStatist + decor;
+    if(numOfStatists > 150){
+        priceClothPerStatist *= 0.9;
+    }
+    
+    if(price > budget){
+        console.log('Not enough money!');
+        console.log(`Wingard needs ${Math.abs((budget-price).toFixed(2))} leva more.`)
+    }
+    else{
+        console.log('Action!');
+        console.log(`Wingard starts filming with ${(budget-price).toFixed(2)} leva left.`)
+    }
+
+
+}
+
+godzillaVsKong(["20000",
+    "120",
+    "55.5"])
+    godzillaVsKong(["15437.62",
+        "186",
+        "57.99"])
+        
+    godzillaVsKong(["9587.88",
+        "222",
+        "55.68"])
+        
+    
