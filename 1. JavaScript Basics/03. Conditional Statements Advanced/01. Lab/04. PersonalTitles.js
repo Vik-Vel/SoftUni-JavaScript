3264b7f475440428ef22c8a8696555071c22606d
Viktoriya Veleva <vikiveleva54@gmail.com>
2024-09-05 21:20:58 +0300

    personalTitles

---
+++ 1. JavaScript Basics/03. Conditional Statements Advanced/01. Lab/04. PersonalTitles.js
@@ -0,0 +1,35 @@
+function personalTitles(input){
+    let age = Number(input[0]);
+    let gender = input[1];
+
+    if(gender === 'm'){
+        if(age >= 16){
+            console.log('Mr.');
+        }
+        else{
+            console.log('Master');
+        }
+    }
+    else{
+        if(age >= 16){
+            console.log('Ms.');
+        }
+        else{
+            console.log('Miss');
+        }
+    }
+}
+personalTitles(["12","f"])
+    
+personalTitles(["17", "m"])
+    
+personalTitles(["25", "f"])
+    
+personalTitles(["13.5", "m"])
+        
+
+// Да се напише функция, която получава възраст (реално число) и пол ('m' или 'f'), въведени от потребителя, и отпечатва обръщение измежду следните:
+// •	"Mr." – мъж (пол 'm') на 16 или повече години
+// •	"Master" – момче (пол 'm') под 16 години
+// •	"Ms." – жена (пол 'f') на 16 или повече години
+// •	"Miss" – момиче (пол 'f') под 16 години
