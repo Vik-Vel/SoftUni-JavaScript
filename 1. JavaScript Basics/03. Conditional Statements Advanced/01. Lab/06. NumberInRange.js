76d7471c3b8b66e387a7c9aa9677fdf7a999f3df
Viktoriya Veleva <vikiveleva54@gmail.com>
2024-09-07 16:30:47 +0300

    06. Number in Range

--- 1. JavaScript Basics/03. Conditional Statements Advanced/01. Lab/06. NumberInRange.js
+++ 1. JavaScript Basics/03. Conditional Statements Advanced/01. Lab/06. NumberInRange.js
@@ -1,7 +1,7 @@
 function numberInRange(input){
     let number = Number(input[0]);
 
-    if(number != 0 && number > -100 && number< 100){
+    if(number != 0 && number >= -100 && number<= 100){
         console.log('Yes');
     }
     else{
