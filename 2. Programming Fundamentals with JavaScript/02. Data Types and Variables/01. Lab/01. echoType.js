1410eae99763f3d8fe6546c3cecaec412349b72a
Viktoriya Veleva <vikiveleva54@gmail.com>
2024-10-03 07:25:17 +0300

    echoType created

---
+++ 2. Programming Fundamentals with JavaScript/02. Data Types and Variables/01. Lab/01. echoType.js
@@ -0,0 +1,16 @@
+function echoType(input){
+    let type = typeof(input);
+
+    if(type === 'number' || type === 'string'){
+        console.log(type);
+        console.log(input);
+    }
+    else{
+        console.log(type);
+        console.log('Parameter is not suitable for printing')
+    }
+   
+}
+
+echoType('Hello, JavaScript!');
+echoType(null)
