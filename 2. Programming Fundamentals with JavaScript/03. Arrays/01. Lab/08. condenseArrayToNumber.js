c6ed887f6949a81f7900f1dbfaf92d564d341fd4
Viktoriya Veleva <vikiveleva54@gmail.com>
2024-11-03 20:03:06 +0200

    Update condenseArrayToNumber

--- 2. Programming Fundamentals with JavaScript/03. Arrays/01. Lab/08. condenseArrayToNumber.js
+++ 2. Programming Fundamentals with JavaScript/03. Arrays/01. Lab/08. condenseArrayToNumber.js
@@ -1,12 +1,22 @@
 function condenseArrayToNumber(array){
     let sum = 0;
-    for(let i = 0; i < array.length - 1; i++){
+    let arrSec = [];
+    
+    if(array.length === 1){
+        sum = Number(array[0]);
+    }
 
-        sum += array[i] + array[i + 1];
+    while(array.length > 1){
+        for(let i = 0; i < array.length - 1; i++){
+            arrSec.push(array[i] + array[i + 1]);
+        }
+        array = arrSec;
+        arrSec = [];
+        sum = Number(array[0]);
     }
     console.log(sum);
 }
 
 //condenseArrayToNumber([2,10,3])
-condenseArrayToNumber([5,0,4,1,2])
+//condenseArrayToNumber([5,0,4,1,2])
 condenseArrayToNumber([1])
