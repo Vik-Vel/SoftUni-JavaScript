f9697a4bcd27ff0184e9aea667124c0ec530bf65
Viktoriya Veleva <vikiveleva54@gmail.com>
2024-11-26 05:34:41 +0200

    Changed  folder

---
+++ 2. Programming Fundamentals with JavaScript/ExamPreparation/01/03. needForSpeedIII.js
@@ -0,0 +1,10 @@
+function needForSpeedIII(input){
+    class Hero{
+        constructor(name, hp, mp){
+            this.name = name;
+            this.hp = Number(hp);
+            this.mp = Number(mp);
+        }
+    }
+
+}
