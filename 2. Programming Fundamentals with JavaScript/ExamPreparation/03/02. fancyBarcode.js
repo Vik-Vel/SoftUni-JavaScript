9d75029cc2fb24fddc69abcaea35568db9d909b7
Viktoriya Veleva <vikiveleva54@gmail.com>
2024-11-30 12:30:51 +0200

    Second task solved

---
+++ 2. Programming Fundamentals with JavaScript/ExamPreparation/03/02. fancyBarcode.js
@@ -0,0 +1,35 @@
+function fancyBarcode(input){
+    const pattern = /^@#+[A-Z][A-Za-z0-9]{4,}[A-Z]@#+$/; // Regular expression for a valid barcode
+    let num = input.shift();
+    
+    for (let i = 0; i < num; i++) {
+        let barcode = input[i];
+
+        // Check if the barcode is valid
+        if (pattern.test(barcode)) {
+            // Extract digits from the barcode
+            let productGroupMatch = barcode.match(/\d+/g);
+            let productGroup = productGroupMatch ? productGroupMatch.join('') : '00';
+
+            console.log(`Product group: ${productGroup}`);
+        } else {
+            console.log(`Invalid barcode`);
+        }
+    }
+   
+}
+
+// fancyBarcode((["3",
+//     "@#FreshFisH@#",
+//     "@###Brea0D@###",
+//     "@##Che4s6E@##"])
+//     )
+
+    fancyBarcode((["6",
+        "@###Val1d1teM@###",
+        "@#ValidIteM@#",
+        "##InvaliDiteM##",
+        "@InvalidIteM@",
+        "@#Invalid_IteM@#",
+        "@#ValiditeM@#"])
+        )
